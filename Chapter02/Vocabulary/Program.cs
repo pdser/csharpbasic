b7ad044656156d8fd4db9c32243a3762cdbece3f
chenkun <chenkunihsn@gmail.com>
2025-05-10 09:54:28 +1200

    chapter02

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -76,7 +76,17 @@ else
 {
 Console.WriteLine($"{a1} + {b} does NOT equal {0.3}");
 }
-
+Console.WriteLine("Using decimals:");
+decimal c = 0.1M; // M suffix means a decimal literal value
+decimal d = 0.2M;
+if (c + d == 0.3M)
+{
+Console.WriteLine($"{c} + {d} equals {0.3M}");
+}
+else
+{
+Console.WriteLine($"{c} + {d} does NOT equal {0.3M}");
+}
     }
 
 }
