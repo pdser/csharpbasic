2bfced3efcd1e8b55033acefe2dfd404ac88ab74
chenkun <chenkunihsn@gmail.com>
2025-05-12 18:46:25 +1200

    code update

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+namespace Formatting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfApples = 12;
+        decimal pricePerApple = 0.35M;
+        Console.WriteLine(
+        format: "{0} apples cost {1:C}",
+        arg0: numberOfApples,
+        arg1: pricePerApple * numberOfApples);
+        string formatted = string.Format(
+        format: "{0} apples cost {1:C}",
+        arg0: numberOfApples,
+        arg1: pricePerApple * numberOfApples);
+        //WriteToFile(formatted); // Writes the string into a file.
+
+        // Three parameter values can use named arguments.
+        Console.WriteLine("{0} {1} lived in {2}.",
+        arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+        // Four or more parameter values cannot use named arguments.
+        Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
+        "Roger", "Cevung", "Stockholm", "Education", "Optimizely");
+        Console.Write("Type your first name and press ENTER: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Type your age and press ENTER: ");
+        string age = Console.ReadLine();
+        Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+
+        Write("Press any key combination: ");
+        ConsoleKeyInfo key = ReadKey();
+        WriteLine();
+        WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+        arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
+
+
+        HttpClient client = new();
+        HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+        WriteLine("Apple's home page has {0:N0} bytes.",
+        response.Content.Headers.ContentLength);
+
+    }
+}
--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Variables;
+﻿using System.Xml;
+namespace Variables;
 
 class Program
 {
@@ -27,6 +28,25 @@ class Program
         Console.WriteLine($"The length of something is {something.Length}");
         // Output the type of the something variable.
         Console.WriteLine($"something is a {something.GetType()}");
-        
+        var population = 67_000_000; // 67 million in UK.
+        var weight = 1.88; // in kilograms.
+        var price = 4.99M; // in pounds sterling.
+        var fruit = "Apples"; // string values use double-quotes.
+        var letter = 'Z'; // char values use single-quotes.
+        var happy = true; // Booleans can only be true or false.
+                          // Good use of var because it avoids the repeated type
+                          // as shown in the more verbose second statement.
+        var xml1 = new XmlDocument(); // Works with C# 3 and later.
+        XmlDocument xml2 = new XmlDocument(); // Works with all C# versions.
+                                              // Bad use of var because we cannot tell the type, so we
+                                              // should use a specific type declaration as shown in
+                                              // the second statement.
+        var file1 = File.CreateText("something1.txt");
+        StreamWriter file2 = File.CreateText("something2.txt");
+        Console.WriteLine($"default(int) = {default(int)}");
+        Console.WriteLine($"default(bool) = {default(bool)}");
+        Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+        Console.WriteLine($"default(string) = {default(string)}");
+
     }
 }
